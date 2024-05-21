8c066a730db7067b62e55db090508840f3f25d1f
Arsh Bangar <arsh.kxn@gmail.com>
2024-05-21 17:03:19 +0530

    New files: Setup and Grub

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -o errexit
+
+CFGS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+CFG_DEFGRUB=/etc/default/grub
+
+cfg_install () {
+    echo "W.I.P"
+}
+
+cfg_backup () {
+    echo "Backup started"
+    if [ -f $CFG_DEFGRUB ]; then
+	cp -i $CFG_DEFGRUB $CFGS_DIR$CFG_DEFGRUB
+	echo "Copied <?> configuration"
+    else
+	echo "<?> configuration does not exist"
+    fi
+}
+
+help_msg () {
+    echo -e "This setup script has two purposes-\n"
+    echo -e "1) Install: Installs the configuration files"
+    echo -e "in this repository to your Linux/GNU system"
+    echo -e "Copies any pre-existing configuration to a"
+    echo -e "directory passed as second argument\n"
+    echo -e "2) Backup: Copies all existing configurations"
+    echo -e "on the system into the current working directory\n"
+    echo -e "Pass these options as first argument (in lowercase)"
+}
+
+case $1 in
+    install)
+	cfg_install ;;
+    
+    backup)
+	cfg_backup ;;
+    
+    *)
+	help_msg ;;
+esac
